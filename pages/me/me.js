6b946931c1f821390d1dfb63dc714fb0b171fb59
温文星 <18475536452@163.com>
2019-02-11 22:46:47 +0800

    员工后台主页

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,18 +1,21 @@
 // pages/me/me.js
+const util = require('../../utils/util');
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    password: null,
+    username: null,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   /**
@@ -62,5 +65,36 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  // 获取用户名
+  getUsername: function (e) {
+    this.setData({
+      username: e.detail.value
+    })
+  },
+
+  // 获取密码
+  getPassword: function (e) {
+    this.setData({
+      password: e.detail.value
+    })
+  },
+
+  // 登陆操作
+  loginClick: function (e) {
+    app.globalData.hafelUserInfo = { username: this.data.username, password: this.data.password }
+    if (app.globalData.hafelUserInfo.username == null && app.globalData.hafelUserInfo.password == null) {
+      util.showSuccess('登录成功');
+      wx.redirectTo({
+        url: '../home/home',
+      })
+    }
+    else {
+      util.showModel('登录失败', '账户或密码错误');
+      this.setData({
+        username: app.globalData.hafelUserInfo.username
+      })
+    }
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,43 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+// 显示繁忙提示
+var showBusy = text => wx.showToast({
+  title: text,
+  icon: 'loading',
+  duration: 6000
+})
+
+// 显示成功提示
+var showSuccess = text => wx.showToast({
+  title: text,
+  icon: 'success'
+})
+
+// 显示失败提示
+var showModel = (title, content) => {
+  wx.hideToast();
+  wx.showModal({
+    title,
+    content: JSON.stringify(content),
+    showCancel: false
+  })
+}
+
+// 不带标题的消息提示
+var showMessage = content => {
+  wx.hideToast();
+  wx.showModal({
+    content: content,
+    showCancel: false,
+    confirmText: "确定"
+  })
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime, 
+  showBusy, 
+  showSuccess, 
+  showModel, 
+  showMessage
 }
